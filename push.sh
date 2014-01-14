af4e8451d274a1695adc9986287f7c3f869b5ff2
Vasudevan Venkateshwaran <vasu.chemical@gmail.com>
2014-01-14 12:12:07 -0500

    Added push script. Minor changes to index.html and publications.html

--- push.sh
+++ push.sh
@@ -8,5 +8,8 @@ jekyll build
 # Push to rcs.rpi.edu
 scp -r ./_site/* venkav2@rcs.rpi.edu:~/public_html/
 
+# Remove the rcs key
+sed -i bkp "/rcs\.rpi\.edu/d" ~/.ssh/known_hosts
+
 # Push to boyle.che.rpi.edu
 # rsync -rvHP ./_site/ venkav2@boyle.che.rpi.edu:~/public_html/
